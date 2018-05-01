47a1b35f3d6394af2d2f505f0217d91700c8edb8
Daniel Blanco <dan16.daniel@gmail.com>
2018-05-01 01:56:08 -0500

    Change decrypt to UTF8

--- EncryptStringTest/Encrypt.cs
+++ EncryptStringTest/Encrypt.cs
@@ -4,15 +4,12 @@ using System.Text;
 using System.Security.Cryptography;
 using System.IO;
 
-//http://stackoverflow.com/questions/10168240/encrypting-decrypting-a-string-in-c-sharp
+
 namespace EncryptStringTest
 {
     public static class Encrypt
     {
-        // This size of the IV (in bytes) must = (keysize / 8).  Default keysize is 256, so the IV must be
-        // 32 bytes long.  Using a 16 character string here gives us 32 bytes when converted to a byte array.
         private const string initVector = "pemgail9uzpgzl88";
-        // This constant is used to determine the keysize of the encryption algorithm.
         private const int keysize = 256;
         //Encrypt
         public static string EncryptString(string plainText, string passPhrase)
@@ -36,7 +33,7 @@ namespace EncryptStringTest
         //Decrypt
         public static string DecryptString(string cipherText, string passPhrase)
         {
-            byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+            byte[] initVectorBytes = Encoding.UTF8.GetBytes(initVector);
             byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
             PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null);
             byte[] keyBytes = password.GetBytes(keysize / 8);
